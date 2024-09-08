fb55df4035d94a41a4e533caaca2804f720cb526
nicodevelop <nicolas@nico-develop.com>
2024-09-08 21:31:17 +0200

    Refactoriser les imports des modèles pour utiliser les chemins appropriés

---
+++ lib/services/abstracts/file_service.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+class FileService {
+  Future<void> createDirectory(String path) async {
+    final Directory dir = Directory(path);
+    if (!(await dir.exists())) {
+      await dir.create(recursive: true);
+    }
+  }
+
+  // TODO: Création de fichier
+  // TODO: Suppression de fichier
+}
